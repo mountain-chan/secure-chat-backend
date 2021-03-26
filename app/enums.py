05088b753e8ca56da9d4222cdd0aac589af43abd
Ly Van Chan <chan.ly@boot.ai>
2021-03-26 10:50:35 +0700

    Push for testing

--- app/enums.py
+++ app/enums.py
@@ -9,4 +9,3 @@ URL_SERVER = "http://localhost:5012" if os.environ.get('DevConfig') == '1' else 
 AVATAR_PATH = "app/files/avatars/"
 AVATAR_PATH_SEVER = URL_SERVER + "/avatars/"
 DEFAULT_AVATAR = "default_avatar.png"
-
