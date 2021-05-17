d53a6184e3aa51cba68bd1a3480033fcb9c77050
mountain-chan <mrchanmta@gmail.com>
2021-05-17 20:16:40 +0700

    update: host server to 54.255.176.27

--- app/enums.py
+++ app/enums.py
@@ -5,7 +5,7 @@ SUPER_ADMIN_ID = "457c17c8-7a74-11eb-9439-0242ac130002"
 
 ALLOWED_EXTENSIONS_IMG = {'png', 'jpg', 'jpeg', 'gif'}
 
-URL_SERVER = "http://localhost:5012" if os.environ.get('DevConfig') == '1' else "http://13.212.239.89:5010"
+URL_SERVER = "http://localhost:5012" if os.environ.get('DevConfig') == '1' else "http://54.255.176.27:5010"
 AVATAR_PATH = "app/files/avatars/"
 AVATAR_PATH_SEVER = URL_SERVER + "/avatars/"
 DEFAULT_AVATAR = "default_avatar.png"
